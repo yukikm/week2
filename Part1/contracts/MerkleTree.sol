a786014998f20d2c2e3d3132fc73be1e465c3af4
kimura-eagency <yuki.kimura.77777@gmail.com>
2022-05-14 02:59:18 +0900

    Part1-2
    Part2-3
    
    Part2-3 update
    
    Part1-3
    
    Part1-3-3

--- Part1/contracts/MerkleTree.sol
+++ Part1/contracts/MerkleTree.sol
@@ -9,12 +9,48 @@ contract MerkleTree is Verifier {
     uint256 public index = 0; // the current index of the first unfilled leaf
     uint256 public root; // the current Merkle root
 
+    uint256[] public parentLevel;
+    uint256 public parentIndex = 0;
+
+    uint256[] public nodes1;
+    uint256[] public nodes2;
+    uint256[] public nodes3;
+
+    // uint256 public itemIndex = 0;
+    // struct Item {
+    //     uint256 index;
+    //     uint256[] data;
+    // }
+
+    // Item[] public items;
+    
     constructor() {
         // [assignment] initialize a Merkle tree of 8 with blank leaves
+        for (uint256 i=0; i < 14; i++){
+            hashes.push(0);
+        }
     }
 
     function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
         // [assignment] insert a hashed leaf into the Merkle tree
+        hashes[index] = hashedLeaf;
+        for (uint256 i = 0; i < 7; i++) {
+            nodes1.push( PoseidonT3.poseidon([hashes[i*2], hashes[i*2+1]]));
+        }
+// nodes1 is seven. Add 1 node.
+        nodes1.push(nodes1[6]);
+        for (uint i = 0; i < 4; i++) {
+            nodes2.push( PoseidonT3.poseidon([nodes1[i*2], nodes1[i*2+1]]));
+        }
+
+// nodes2 is four
+        for (uint i = 0; i < 2; i++) {
+            nodes3.push( PoseidonT3.poseidon([nodes1[i*2], nodes1[i*2+1]]));
+        }
+
+        index++;
+        root = PoseidonT3.poseidon([nodes3[0], nodes3[1]]);
+        return root;
     }
 
     function verify(
@@ -25,5 +61,6 @@ contract MerkleTree is Verifier {
         ) public view returns (bool) {
 
         // [assignment] verify an inclusion proof and check that the proof root matches current root
+        return Verifier.verifyProof(a, b, c, input);
     }
 }
